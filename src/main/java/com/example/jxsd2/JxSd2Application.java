64ac571e147bce67297bb01888e382a55be9816e
Troy Hart <thart@myriad.com>
2020-04-20 10:10:55 -0600

    initial commit

---
+++ src/main/java/com/example/jxsd2/JxSd2Application.java
@@ -0,0 +1,13 @@
+package com.example.jxsd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxSd2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxSd2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jxsd2/JxSd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jxsd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxSd2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
